861b30a4e95d2c4715bd7317e9f34c97e3a3ae5a
Jay Rosenthal <jayrosenthal@US-L379P63DWC.local>
2022-03-29 05:10:43 -0700

    working for product board and failing on an Asana API error

--- plugin_integration.py
+++ plugin_integration.py
@@ -1,9 +1,12 @@
+import json
 import os
 import requests
 
 PRODUCT_BOARD_API_KEY = os.getenv("PRODUCT_BOARD_JAY_WALL_TEST_INTEGRATION")
-headers = {'Authorization': 'Bearer ' + PRODUCT_BOARD_API_KEY, 'X-Version': '1'}
-
+ASANA_CLIENT_SECRET = os.getenv("ASANA_TEST_INTEGRATION_CLIENT_SECRET")
+headers = {'Authorization': 'Bearer ' + PRODUCT_BOARD_API_KEY,
+           'X-Version': '1',
+           'Content-Type': 'application/json'}
 
 def get_features_list():
     r = requests.get("https://api.productboard.com/features", headers=headers)
@@ -17,33 +20,37 @@ def get_features_list():
 
     return list
 
-ASANA_CLIENT_SECRET = os.getenv("ASANA_TEST_INTEGRATION_CLIENT_SECRET")
+def create_plugin_integration():
+    integration_settings = {
+                      "data": {
+                        "integrationStatus": "enabled",
+                        "type": "com.asana.app",
+                        "name": "Asana",
+                        "initialState": {
+                          "label": "Push"
+                        },
+                        "action": {
+                          "url": "https://app.asana.com/api/1.0/tasks",
+                          "version": 1,
+                          "headers": {
+                            "authorization": ASANA_CLIENT_SECRET
+                          }
+                        }
+                      }
+                    }
 
-integration_settings = {
-  "data": {
-    "integrationStatus": "enabled",
-    "type": "com.asana.app",
-    "name": "Asana",
-    "initialState": {
-      "label": "Push"
-    },
-    "action": {
-      "url": "https://app.asana.com/api/1.0/tasks",
-      "version": 1,
-      "headers": {
-        "authorization": ASANA_CLIENT_SECRET
-      }
-    }
-  }
-}
+    r = requests.post('https://api.productboard.com/plugin-integrations', data=json.dumps(integration_settings), headers=headers)
+    print(r.text)
+
+    return None
 
 
 if __name__ == '__main__':
-    get_features_list()
+    create_plugin_integration()
+    # get_features_list()
 
 
-# r = requests.post('https://api.productboard.com/plugin-integrations', data=integration_settings, headers=headers)
-# print(r.text)
+
 
 
 
