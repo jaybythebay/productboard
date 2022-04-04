b95e3ceae427fb7f2ac60d13f127ffb2001ba964
Jay Rosenthal <jayrosenthal@US-L379P63DWC.local>
2022-04-04 06:19:39 -0700

    Dumping headers and getting different error now

--- plugin_integration.py
+++ plugin_integration.py
@@ -5,11 +5,15 @@ import asana
 
 PRODUCT_BOARD_API_KEY = os.getenv("PRODUCT_BOARD_JAY_WALL_TEST_INTEGRATION")
 ASANA_PERSONAL_ACCESS_TOKEN = os.getenv("ASANA_PRODUCT_BOARD_TEST_2")
+
 product_board_headers = {'Authorization': 'Bearer ' + PRODUCT_BOARD_API_KEY,
            'X-Version': '1',
            'Content-Type': 'application/json'}
+
 client = asana.Client.access_token(ASANA_PERSONAL_ACCESS_TOKEN)
-asana_headers = {'Accept': 'application/json', 'Authorization': 'Bearer ' + ASANA_PERSONAL_ACCESS_TOKEN}
+asana_headers = {'Accept': 'application/json',
+                 'Content-Type': 'application/json',
+                 'Authorization': 'Bearer ' + ASANA_PERSONAL_ACCESS_TOKEN}
 
 
 def get_features_list():
@@ -72,11 +76,14 @@ def create_plugin_integration():
                           "url": "https://app.asana.com/api/1.0/tasks",
                           "version": 1,
                           "headers": {
-                            "authorization": asana_headers
+                            "authorization": json.dumps(asana_headers)
                           }
                         }
                       }
                     }
+
+    # r = requests.post('https://api.productboard.com/plugin-integrations',
+    #                   data=integration_settings, headers=product_board_headers)
 
     r = requests.post('https://api.productboard.com/plugin-integrations', data=json.dumps(integration_settings), headers=product_board_headers)
     print(r.text)
@@ -85,11 +92,11 @@ def create_plugin_integration():
 
 
 if __name__ == '__main__':
-    # create_plugin_integration()
+    create_plugin_integration()
     # get_features_list()
     # asana_get_tasks_list()
     # create_plugin_integration()
-    asana_test_authentication_with_requests()
+    # asana_test_authentication_with_requests()
 
 
 
